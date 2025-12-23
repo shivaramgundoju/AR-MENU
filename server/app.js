13094f869af0cdf9d2bd3e7b773ba4656a3a3736
shivaramgundoju <shivaramgundoju@gmail.com>
2025-12-23 23:58:50 +0530

    Fix Render crash by removing static model path

--- server/app.js
+++ server/app.js
@@ -1,23 +1,17 @@
 // server/app.js
 import express from "express";
 import cors from "cors";
-import path from "path";
-import { fileURLToPath } from "url";
 import dishRoutes from "./routes/dishRoutes.js";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// Serve static files from client/public directory
-app.use('/models', express.static(path.join(__dirname, '../client/public/models')));
+app.get("/", (req, res) => {
+  res.send("AR Menu Backend Running");
+});
 
 app.use("/api", dishRoutes);
 
-// Export both default and named to satisfy any import style
-export { app };
 export default app;
